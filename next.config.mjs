24fa5392b4039c94b82989631881c4bed7d45c09
aojunhao123 <1844749591@qq.com>
2026-03-01 20:28:29 +0800

    fix(docs): copy markdown not working (#60)
    - copy markdown not working
    - adjust the file directory location
    - add loading indicator

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:language/docs.mdx',
+        destination: '/:language/llms.mdx/docs',
+      },
+      {
+        source: '/:language/docs/:slug*.mdx',
+        destination: '/:language/llms.mdx/docs/:slug*',
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
